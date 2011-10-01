4454741007acdb72762f888c4383cd90d306e9e1
Alexander Glushkov <cutalion@gmail.com>
2011-10-02 01:47:32 +0300

    Increase default alphabet; move it to constant

--- lib/encode_with_alphabet.rb
+++ lib/encode_with_alphabet.rb
@@ -1,25 +1,26 @@
 require "encode_with_alphabet/version"
 
 module EncodeWithAlphabet
-    def self.encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-      raise 'number must be an integer' unless number.is_a? Fixnum
-      raise 'alphabet must be non-empty' if alphabet.empty?
+  ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  def self.encode(number, alphabet = ALPHABET)
+    raise 'number must be an integer' unless number.is_a? Fixnum
+    raise 'alphabet must be non-empty' if alphabet.empty?
 
-      return alphabet[0,1] if number == 0
+    return alphabet[0,1] if number == 0
 
-      base = ''
-      sign = ''
+    base = ''
+    sign = ''
 
-      if number < 0
-        sign = '-'
-        number = - number
-      end
-
-      while number != 0
-        number, i = number.divmod alphabet.size
-        base = alphabet[i, 1] + base
-      end
-
-      return sign + base
+    if number < 0
+      sign = '-'
+      number = - number
     end
+
+    while number != 0
+      number, i = number.divmod alphabet.size
+      base = alphabet[i, 1] + base
+    end
+
+    return sign + base
+  end
 end
